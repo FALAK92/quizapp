30cda4da6a59560ef79ac879862960249646a9c3
FALAK92 <naaz92falak@gmail.com>
2024-09-29 20:47:26 +0500

    quiz app

---
+++ question.js
@@ -0,0 +1,107 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Home Text Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "A. Cascading Style Sheet",
+    options: [
+      "A. Cascading Style Sheet",
+      "B. Cute Style Sheet",
+      "C. Computer Style Sheet",
+      "D. Compress Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "Which of the following is a JavaScript framework?",
+    answer: "B. Angular",
+    options: ["A. Django", "B. Angular", "C. Ruby on Rails", "D. Flask"],
+  },
+  {
+    numb: 4,
+    question: "Which CSS property is used to change the text color of an element?",
+    answer: "A. color",
+    options: [
+        "A. color",
+        "B. text-color",
+        "C. font-color",
+        "D. background-color",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does API stand for?",
+    answer: "A. Application Programming Interface",
+    options: [
+        "A. Application Programming Interface",
+        "B. Application Programming Internet",
+        "C. Application Protocol Interface",
+        "D. Advanced Programming Interface",
+    ],
+  },
+  {
+    numb: 6,
+    question: "Which platform is primarily used for Android app development?",
+    answer: "B. Kotlin",
+    options: [
+        "A. Swift",
+        "B. Kotlin",
+        "C. Objective-C",
+        "D. Javascript",
+    ],
+  },
+  {
+    numb: 7,
+    question: "What is the purpose of the <meta> tag in HTML?",
+    answer: "A. To define metadata about the HTML document",
+    options: [
+        "A. To define metadata about the HTML document",
+        "B. To link to external scripts",
+        "C. To style the page",
+        "D. To create forms",
+    ],
+  },
+  {
+    numb: 8,
+    question: "In JavaScript, what does the === operator do?",
+    answer: "B. Compares values and types for equality",
+    options: [
+        "A. Compares values for equality with type coercion",
+        "B. Compares values and types for equality",
+        "C. Assign a value to a variable",
+        "D. Checks if a variable is undefined",
+    ],
+  },
+  {
+    numb: 9,
+    question: "Which of the following is a popular CSS framework?",
+    answer: "C. Bootstrap",
+    options: [
+        "A. React",
+        "B. Angular",
+        "C. Bootstrap",
+        "D. Node.js",
+    ],
+  },
+  {
+    numb: 10,
+    question: "What will the following JavaScript code output? console.log(typeof null);",
+    answer: "B. object",
+    options: [
+        "A. null",
+        "B. object",
+        "C. undefined",
+        "D. NaN",
+    ],
+  },
+];
